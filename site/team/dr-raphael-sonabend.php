0bd2115cfbb8c3a1ab9be0a2f40f0a37329e0a6d
RaphaelS1 <r.sonabend@osponow.com>
2024-01-17 10:49:37 +0000

    Update from https://github.com/osponow/website_dev/commit/6c04d9ac60ae96b8c30abcad1a273109e63e6ab8

--- site/team/dr-raphael-sonabend.php
+++ site/team/dr-raphael-sonabend.php
@@ -13,17 +13,17 @@
 	$pronouns = "They/He";
 	$image = "raphael.jpeg";
 	$linkedin = "raphael-sonabend";
-	$content = "<p>Dr Raphael Sonabend is a Director and Co-Founder of OSPO Now.
+	$content = "<p>Dr Raphael Sonabend is the CEO and and Co-Founder of OSPO Now.
 They have over 10 years of experience working in data analysis, data science, and open source, consulting for companies including LEGO, Nuffield Health, ISEH, and Roche on behalf of the Alan Turing Institute.
 Raphael oversees all work at OSPO Now as well as working on our strategy and growth.</p>
 <p>Prior to founding OSPO Now, Raphael worked at Genomics England developing an Open Source community for tools to enhance genomic equity.
 Before Genomics England, they worked at the Wellcome Trust, focused on funding high-impact open-source, data science projects for tackling urgent global health problems.
 Whilst at Wellcome, Raphael designed, created, and launched the inaugural Wellcome Data Science Ideathon, which was a novel mechanism to award funding more equitably to early-career researchers.
-During the height of the COVID-19 pandemic, Raphael worked in Professor Neil Ferguson's COVID-19 modelling team, including developing code and reporting results to SPI-M-O, the modelling subgroup of SAGE, for which they received a <a href='https://www.imperial.ac.uk/news/240776/imperial-real-time-modelling-team-receives-award/'>SAMDS award</a>.</p>
+During the height of the COVID-19 pandemic, Raphael worked in Professor Neil Ferguson's COVID-19 modelling team, including developing code and reporting results that helped inform the government response to lifting lockdown restrictions, for which they received a <a href='https://www.imperial.ac.uk/news/240776/imperial-real-time-modelling-team-receives-award/'>SAMDS award</a>.</p>
 <p>Raphael received a <a href='https://discovery.ucl.ac.uk/id/eprint/10129352/'>PhD in Statistics</a> from UCL and has authored papers in high-impact journals including the Lancet, Nature, and Science.
 They also have first-hand experience of developing open-source packages, including software for machine learning, symbolic programming, and object-oriented programming, across R, Julia, and Python.
 They co-authored and edited the book <a href='https://mlr3book.mlr-org.com/'>Applied Machine Learning Using mlr3 in R</a> published by CRC Press.
-Raphael's primary research interests are in survival analysis, with a book on <a href='https://www.mlsabook.com/'>Machine Learning in Survival Analysis</a> due to be published by CRC in January 2025.
+Raphael's primary research interests are in survival analysis, with a book on <a href='https://www.mlsabook.com/'>Machine Learning in Survival Analysis</a> due to be published by CRC Press in January 2025.
 Their academic articles can be found on their <a href='https://scholar.google.co.uk/citations?user=pNiqz8kAAAAJ&amp;hl=en'>Google Scholar page</a> and code can be perused <a href='https://github.com/RaphaelS1'>on their GitHub</a>.</p>";
 	
       include_once $path . '/shared/team_template.php';
