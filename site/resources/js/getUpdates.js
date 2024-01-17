36f8dcb3a377ff0de3f17f0ffbd39ed8903588d1
RaphaelS1 <r.sonabend@osponow.com>
2024-01-17 10:37:17 +0000

    Update from https://github.com/osponow/website_dev/commit/0a4a1d162a07cb3902377d5925d5796662bb02eb

--- site/resources/js/getUpdates.js
+++ site/resources/js/getUpdates.js
@@ -1,1 +1,1 @@
-function getUpdates(a){const e=[{name:"OSPO Now has launched",datestring:"19 January 2024",image:"logo.png",imagealt:"OSPO Now logo.",type:"News",date:"2024-01-19",authors:["Dr Raphael Sonabend","Dr Agnes Kiragga"]}];return void 0===a?e:e.filter((e=>e.name==a))[0]}
+function getUpdates(a){const e=[{name:"OSPO Now has launched",datestring:"17 January 2024",image:"logo.png",imagealt:"OSPO Now logo.",type:"News",date:"2024-01-17",authors:["Dr Raphael Sonabend","Dr Agnes Kiragga"]}];return void 0===a?e:e.filter((e=>e.name==a))[0]}
