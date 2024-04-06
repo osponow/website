b7401b25377b4ffa6368f9ca0bb2e1451172f802
RaphaelS1 <r.sonabend@osponow.com>
2024-04-06 10:08:35 +0000

    Update from https://github.com/osponow/website_dev/commit/661faaab8b2caf3686438ae95e1d6dc5650af1b3

--- site/team/michelle-barker.php
+++ site/team/michelle-barker.php
@@ -4,16 +4,16 @@
 
     <?php
       $path = $_SERVER['DOCUMENT_ROOT'];
-      $title = "Michelle Barker";
+      $title = "Dr Michelle Barker";
       $header = "white";
-      $name = "Michelle Barker";
+      $name = "Dr Michelle Barker";
 	$name_abbr = "Barker, M";
 	$email = "None";
 	$jobtitle = "Consultant";
 	$pronouns = "She/Her";
 	$image = "michelle.png";
 	$linkedin = "michelledbarker";
-	$content = "<p>Michelle works both as an <a href='https://www.linkedin.com/in/michelledbarker/'>Open Science Consultant</a>, and as the Director of the <a href='https://www.researchsoft.org/'>Research Software Alliance</a>. As a sociologist, Michelle is passionate about building collaborative partnerships to achieve system change, and she brings extensive expertise in leading strategic and policy initiatives.</p>
+	$content = "<p>Dr Michelle Barker works both as an <a href='https://www.linkedin.com/in/michelledbarker/'>Open Science Consultant</a>, and as the Director of the <a href='https://www.researchsoft.org/'>Research Software Alliance</a>. As a sociologist, Michelle is passionate about building collaborative partnerships to achieve system change, and she brings extensive expertise in leading strategic and policy initiatives.</p>
 <p>She serves on advisory boards for <a href='https://fair-impact.eu/'>FAIR Impact</a>, <a href='https://everse.software/'>European Virtual Institute for Research Software Excellence</a> (EVERSE), <a href='https://rse-asia.github.io/RSE_Asia/aboutus.html'>Research Software Engineers Asia Association</a>, and the <a href='https://rd-alliance.org/about-rda/our-leadership/rda-organisational-advisory-board.html'>Research Data Alliance Organisational Advisory Board</a>. As a <a href='https://www.linkedin.com/in/michelledbarker/'>consultant</a>, her roles have included co-editing <a href='http://10.0.20.161/zenodo.10473185'>Software and skills for research computing in the UK</a>; and the European Open Science Cloud report, <a href='https://www.eoscsecretariat.eu/news-opinion/digital-skills-fair-open-science-report-eosc-skills-training-working-group'>Digital Skills for FAIR and Open Science</a>.</p>
 <p>Michelle is a former Director of the <a href='https://ardc.edu.au/'>Australian Research Data Commons</a> (ARDC), where she led the strategic planning for the Australian government’s $180 million, five-year investment in ARDC, and the <a href='https://nectar.org.au/labs/'>national research software infrastructure investment program</a>.</p>";
 	
