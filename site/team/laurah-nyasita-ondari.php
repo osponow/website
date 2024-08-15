8d5473bb281e34b9c26b47a6d54322c1e06da0bb
RaphaelS1 <r.sonabend@osponow.com>
2024-08-15 09:36:04 +0000

    Update from https://github.com/osponow/website_dev/commit/5a9e63c313ac9fe7d560abda91021ae9557ef508

---
+++ site/team/laurah-nyasita-ondari.php
@@ -0,0 +1,25 @@
+
+    <!DOCTYPE html>
+    <html lang="en">
+
+    <?php
+      $path = $_SERVER['DOCUMENT_ROOT'];
+      $title = "Laurah Nyasita Ondari";
+      $header = "white";
+      $name = "Laurah Nyasita Ondari";
+	$name_abbr = "Ondari, L";
+	$email = "None";
+	$jobtitle = "Open Source Manager";
+	$pronouns = "She/Her";
+	$image = "laurah.png";
+	$linkedin = "laurah-ondari-749175b6";
+	$content = "<p>Nyasita is a bioinformatician with a strong focus on applying computational techniques to address complex biological questions.  With a solid academic foundation, including an MSc in Bioinformatics and a BSc in Medical Biochemistry, she has cultivated extensive experience working with various omics data.</p>
+<p>In addition to technical expertise, Nyasita is deeply committed to community engagement and capacity development within the bioinformatics field. She is a co-founder of the <a href='https://bhki.org/'>Bioinformatics Hub of Kenya Initiative (BHKi)</a>, a volunteer-run and non-profit organization that plays a vital role in advancing bioinformatics in Kenya by fostering a collaborative environment where experienced and emerging bioinformaticians can connect, share knowledge, and mentor one another.</p>
+<p>Nyasita also launched <a href='https://open.spotify.com/show/4TWZ7HkUvgFYzwnSkOK7gG?si=25230318275d4953'>the Science In Real Life Podcast (SIRL)</a> in 2023, to address the critical need for relatable STEM journey stories. Through in-depth interviews and personal stories, the podcast explores how STEM professionals have navigated their careers and remained committed to their fields, providing inspiration and insights for the next generation of STEM leaders.</p>
+<p>Outside of professional pursuits, Nyasita is actively involved in various passion projects, including volunteering with the Deep Learning Indaba, Open Life Science, and TEDxParklands.</p>";
+	
+      include_once $path . '/shared/team_template.php';
+    ?>
+
+    </html>
+    
