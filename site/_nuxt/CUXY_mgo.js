c88817ea6e49b68e6cd6a78473d9f3e6bc7d8ecd
RaphaelS1 <r.sonabend@osponow.com>
2025-07-18 15:50:25 +0000

    Update from https://github.com/osponow/website_dev/commit/6861f7d0ea3c3c70560946b45b7b22426a1b1182

--- site/_nuxt/CUXY_mgo.js
+++
@@ -1,1 +0,0 @@
-import{_ as o,c as r,o as t,v as s}from"./C7HLD6N3.js";const a={};function c(e,n){return t(),r("table",null,[s(e.$slots,"default")])}const _=o(a,[["render",c]]);export{_ as default};
